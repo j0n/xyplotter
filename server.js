466941115fd62514cea53fd483c802794147b6db
Jon <andersson.jon@gmail.com>
2016-02-18 22:51:11 +0100

    support save paths

--- server.js
+++ server.js
@@ -7,12 +7,29 @@ app.use(express.static('dist'));
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-var xy = {}// require('./index');
+var xy = require('./index');
 
 
-app.post('/saveObject/:name', (req, res) => {
+app.post('/saveObject/:name/:threshold?', (req, res) => {
     xyUtil.save(req.params.name, req.body);
     res.send('Done')
+})
+app.get('/yrepeat/:name/:x/:y/:amount/:size', (req, res) => {
+    for (var i = 0; i < req.params.amount * 1; i++) {
+        var paths = xyUtil.load(req.params.name).paths.map((path) => {
+            return xyUtil.scale(req.params.scale || 0.025, path)
+        })
+        console.log('x', req.params.x + (i * req.params.size));
+        xy.path((1 * req.params.x) + (i * req.params.size), req.params.y, paths.slice(0))
+    }
+    res.send('addes');
+})
+app.get('/draw/:name/:x/:y/:scale?', (req, res) => {
+    var paths = xyUtil.load(req.params.name).paths.map((path) => {
+        return xyUtil.scale(req.params.scale || 0.2, path)
+    })
+    xy.path(req.params.x, req.params.y, paths)
+    res.send('addes');
 })
 
 app.get('/d/:value', (req, res) => {
@@ -23,10 +40,6 @@ app.get('/u/:value', (req, res) => {
   xy.upValue(req.params.value);
   res.send(xy.info());
 })
-app.get('/start', (req, res) => {
-  xy.next();
-  res.send('start');
-});
 
 app.get('/add/:x/:y/:amount/:min/:max', (req, res) => {
   xy.addEntitiy(
@@ -46,6 +59,10 @@ app.get('/home', (req, res) => {
 app.get('/go/:x/:y', (req, res) => {
   xy.go(req.params.x, req.params.y);
   res.send('go');
+});
+app.get('/pattern/:x/:y/:width/:height', (req, res) => {
+  xy.pattern(req.params.x, req.params.y, req.params.width, req.params.height);
+  res.send('test');
 });
 app.get('/test/:x/:y/:n/:r', (req, res) => {
   xy.test(req.params.x, req.params.y, req.params.n, req.params.r);
@@ -72,5 +89,26 @@ app.get('/abort', (req, res) => {
 app.get('/info', (req, res) => {
   res.send(xy.info());
 })
+app.get('/pause', (req, res) => {
+    xy.pause();
+    res.end('ok');
+})
+app.get('/play', (req, res) => {
+    xy.pause(false);
+    xy.next();
+    res.end('ok');
+})
+app.get('/start', (req, res) => {
+  xy.next();
+  res.send('start');
+});
+app.get('/down', (req, res) => {
+  xy.goDown();
+  res.send('down');
+});
+app.get('/up', (req, res) => {
+  xy.goUp();
+  res.send('up');
+});
 
 app.listen(4444);
