b64904407ecfa32ef4472ac0e6b75dcea0718dd0
Jon Andersson <andersson.jon@gmail.com>
2016-02-18 23:29:56 +0100

    clean up and start better on pattern

--- app/js/app.js
+++ app/js/app.js
@@ -17,69 +17,42 @@ window.onload = function () {
         })
 
 
-        $('play').addEventListener('click', () => {
-            fetch('/play')
-        })
-        $('pause').addEventListener('click', () => {
-            fetch('/pause')
-        })
+    $('play').addEventListener('click', () => {
+        fetch('/play')
+    })
+    $('pause').addEventListener('click', () => {
+        fetch('/pause')
+    })
+
     document.getElementById('home').addEventListener('click', () => {
         fetch('/go/0/0')
     })
-  commandEl = document.getElementById('commandos');
-  document.getElementById('my').addEventListener('submit', function(e) {
-    e.preventDefault();
-    commandos.push(document.getElementById('url').value);
-    renderCommandos();
-    fetch(document.getElementById('url').value)
-    .then((res) => {
-      console.log(res);
-    })
-    .catch((err) => {
-      console.log(err);
-    })
+    commandEl = document.getElementById('commandos');
+    document.getElementById('my').addEventListener('submit', function (e) {
+        e.preventDefault();
+        commandos.push(document.getElementById('url').value);
+        renderCommandos();
+        fetch(document.getElementById('url').value)
+        .then((res) => {
+            console.log(res);
+        })
+        .catch((err) => {
+            console.log(err);
+        })
 
-    console.log('submit');
-  })
+        console.log('submit');
+    })
 }
 
 function renderCommandos () {
-  var commandoDiv = document.createElement('div');
-  commandoDiv.innerHTML = commandos.pop();
-  if (commandEl.childNodes.length > 0) {
-    commandEl.insertBefore(commandoDiv,commandEl.childNodes[0]);
-  }
-  else {
-    commandEl.appendChild(commandoDiv);
-  }
-  if (commandos.length > 0) {
-    renderCommandos();
-  }
-}
-
-var fromSVG = function (svg) {
-    return fetch(svg)
-        .then((obj) => {
-            return obj.text().then((data) => {
-                var holder = document.getElementById('svg-holder');
-                holder.innerHTML = data;
-                var paths = Array.prototype.slice.call(
-                                holder.querySelectorAll('path')
-                            );
-                return paths.map((path) => {
-                    var points = [];
-                    window.p = path;
-                    for (var i = 0, ii = path.getTotalLength(); i < ii; i++) {
-                        var svgPoint = path.getPointAtLength(i)
-                        points.push({
-                            x: svgPoint.x,
-                            y: svgPoint.y
-                        })
-                    }
-                    holder.innerHTML = '';
-                    return points;
-                })
-
-            });
-        })
+    var commandoDiv = document.createElement('div');
+    commandoDiv.innerHTML = commandos.pop();
+    if (commandEl.childNodes.length > 0) {
+        commandEl.insertBefore(commandoDiv, commandEl.childNodes[0]);
+    } else {
+        commandEl.appendChild(commandoDiv);
+    }
+    if (commandos.length > 0) {
+        renderCommandos();
+    }
 }
